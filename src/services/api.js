a8b6730092e53df8c6dbfffceb6627ae904da441
Upwarp <159637736+Upwarp@users.noreply.github.com>
2026-05-18 12:58:37 -0500

    corrección ruta

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://gestilibro1-production.up.railway.app/api/";
+const API_BASE_URL = "https://gestilibro1-production.up.railway.app/api";
 
 export async function apiRequest(endpoint, options = {}) {
   const token = localStorage.getItem("token");
