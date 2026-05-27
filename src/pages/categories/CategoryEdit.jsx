4a688db981b8e49fcbb844612385b4ffcb26c605
Upwarp <159637736+Upwarp@users.noreply.github.com>
2026-05-26 20:21:41 -0500

    Update CategoryEdit.jsx

--- src/pages/categories/CategoryEdit.jsx
+++ src/pages/categories/CategoryEdit.jsx
@@ -96,7 +96,7 @@ export default function CategoryEdit() {
             />
 
             <FormActions
-              cancelTo="/categories"
+              cancelTo="/categorias"
               submitLabel="Actualizar"
               loadingLabel="Actualizando..."
               loading={saving}
