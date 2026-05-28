f997581d82f6f847250a4cc6408d985a0f45f6fd
Upwarp <159637736+Upwarp@users.noreply.github.com>
2026-05-28 18:42:41 -0500

    Ayuda para el redireccionamiento

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import PrivateRoute from "./PrivateRoute";
+import RoleRoute from "./RoleRoute";
+import RoleHome from "./RoleHome";
 
 import Login from "../pages/Login";
 import Dashboard from "../pages/Dashboard";
